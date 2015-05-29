5b050d03a6ac6593e6a259ca95236f7d110ecc74
sabotuer99 <sabotuer99@gmail.com>
2015-05-28 23:38:42 -0300

    added rotten potatoes code

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rottenpotatoes::Application.config.secret_token = '447f6666dd5485ba0c75fadbd5116de585c0c4d765f3e613ec16370029c91f9fc253d3d87e03ce472bd382aacfde7975e6e00ba220947e710f5ef8e899b7dcec'

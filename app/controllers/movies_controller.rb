2e21c600b6c51feac717a13957375f15687654f1
sabotuer99 <sabotuer99@gmail.com>
2015-06-07 02:19:03 -0300

    Added sorting movies by title and release date

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -7,7 +7,13 @@ class MoviesController < ApplicationController
   end
 
   def index
-    @movies = Movie.all
+	if params[:sort] == "title" 
+		@movies = Movie.all.sort_by{|e| e[:title]}
+	elsif params[:sort] == "release_date" 
+		@movies = Movie.all.sort_by{|e| e[:release_date]}
+	else
+		@movies = Movie.all
+	end
   end
 
   def new

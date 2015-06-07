5d8028bd777774a32a3e2637e4fd0b7d8d80ce39
sabotuer99 <sabotuer99@gmail.com>
2015-06-07 00:57:07 -0300

    Initial setup complete

--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,8 @@ Rottenpotatoes::Application.routes.draw do
 
   # Sample resource route (maps HTTP verbs to controller actions automatically):
   #   resources :products
+	resources :movies
+
 
   # Sample resource route with options:
   #   resources :products do
@@ -49,6 +51,7 @@ Rottenpotatoes::Application.routes.draw do
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
   # root :to => 'welcome#index'
+	root :to => 'movies#index'
 
   # See how all your routes lay out with "rake routes"
 

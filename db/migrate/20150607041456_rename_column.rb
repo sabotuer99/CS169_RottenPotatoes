0e6490c074f4e0ae6620271e4b60f424b9ad9694
sabotuer99 <sabotuer99@gmail.com>
2015-06-07 01:32:56 -0300

    Fixed the migration...

--- db/migrate/20150607041456_rename_column.rb
+++
@@ -1,7 +0,0 @@
-class RenameColumn < ActiveRecord::Migration
-  def up
-  end
-
-  def down
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,11 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20150607041456) do
+ActiveRecord::Schema.define(:version => 20150607042730) do
 
   create_table "movies", :force => true do |t|
     t.string   "title"
-    t.datetime "releaase_date"
+    t.datetime "release_date"
     t.string   "rating"
     t.text     "description"
   end
